fd116e20cb8fc1dbb3ed8041924ccc46aa3e9fbf
Michael <hellercmichael@gmail.com>
2022-07-29 11:05:56 -0400

    Show info received from the tauri commands in the UI.

--- cn-gui/src/App.tsx
+++ cn-gui/src/App.tsx
@@ -1,38 +1,53 @@
-import { ReactElement, useState } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import { invoke } from '@tauri-apps/api/tauri'
 
 import logo from './logo.svg';
 import './App.css';
 
+interface IColourInfo {
+  hex: String;
+  rgb: String;
+  hsl: String;
+  name: String;
+}
+
 function App(): ReactElement<any, any> {
-  const [colourInfo, setColourInfo] = useState('');
+  const [inputValue, setInputValue] = useState<string>('');
+  const [colourInfo, setColourInfo] = useState<IColourInfo | null>(null);
 
   const onConvertHex = async () => {
     try {
-      const response = await invoke('convert_hex_string', {
-        hex: colourInfo
+      const response = await invoke<IColourInfo>('convert_hex_string', {
+        hex: inputValue
       });
 
-      console.log(response);
+      setColourInfo(response);
     } catch (err) {
       console.error(err);
     }
   }
 
+  useEffect(() => {
+    onConvertHex();
+  }, [inputValue]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <input
-          value={colourInfo}
+          value={inputValue}
           placeholder="hex"
-          onChange={(event) => setColourInfo(event.target.value)}
+          onChange={(event) => setInputValue(event.target.value)}
         />
-        <button
-          className="btn"
-          onClick={onConvertHex}>
-          Convert Hex
-        </button>
+
+        <div>
+          <pre>hex  - {colourInfo?.hex}</pre>
+          <pre>rgb  - {colourInfo?.rgb}</pre>
+          <pre>hsl  - {colourInfo?.hsl}</pre>
+          <pre>name - {colourInfo?.name}</pre>
+        </div>
       </header>
     </div>
   );
