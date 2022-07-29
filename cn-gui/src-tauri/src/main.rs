d29a9db326ea55425bb9a8ef404ecfd23a774623
Michael <hellercmichael@gmail.com>
2022-07-28 23:50:50 -0400

    Make tauri commands return Option.

--- cn-gui/src-tauri/src/main.rs
+++ cn-gui/src-tauri/src/main.rs
@@ -15,55 +15,53 @@ struct ColourInfo {
 }
 
 #[tauri::command]
-fn convert_hex_string(hex: String) -> ColourInfo {
-    match ColourNaming::to_hex_value_from_hex_string(hex.as_str()) {
-        Ok(hex_value) => {
-            let rgb_bytes = ColourNaming::to_rgb_bytes_from_hex_value(hex_value);
-            let hsl_ratios = ColourNaming::to_hsl_ratios_from_rgb_bytes(rgb_bytes);
-            let hsl_values = ColourNaming::to_hsl_values_from_hsl_ratios(hsl_ratios);
+fn convert_hex_string(hex: String) -> Option<ColourInfo> {
+    if let Ok(hex_value) = ColourNaming::to_hex_value_from_hex_string(hex.as_str()) {
+        let rgb_bytes = ColourNaming::to_rgb_bytes_from_hex_value(hex_value);
+        let hsl_ratios = ColourNaming::to_hsl_ratios_from_rgb_bytes(rgb_bytes);
+        let hsl_values = ColourNaming::to_hsl_values_from_hsl_ratios(hsl_ratios);
 
-            ColourInfo {
-                hex: ColourNaming::to_hex_string_from_rgb_bytes(rgb_bytes),
-                rgb: ColourNaming::to_rgb_string_from_rgb_bytes(rgb_bytes),
-                hsl: ColourNaming::to_hsl_string_from_hsl_values(hsl_values),
-                name: ColourNaming::to_name_from_rgb_bytes(rgb_bytes),
-            }
-        },
-        Err(err) => panic!("{:?}", err),
+        return Some(ColourInfo {
+            hex: ColourNaming::to_hex_string_from_rgb_bytes(rgb_bytes),
+            rgb: ColourNaming::to_rgb_string_from_rgb_bytes(rgb_bytes),
+            hsl: ColourNaming::to_hsl_string_from_hsl_values(hsl_values),
+            name: ColourNaming::to_name_from_rgb_bytes(rgb_bytes),
+        });
     }
+
+    None
 }
 
 #[tauri::command]
-fn convert_name_string(name: String) -> ColourInfo {
-    match ColourNaming::to_hex_value_from_name(name.as_str()) {
-        Ok(hex_value) => {
-            let rgb_bytes = ColourNaming::to_rgb_bytes_from_hex_value(hex_value);
-            let hsl_ratios = ColourNaming::to_hsl_ratios_from_rgb_bytes(rgb_bytes);
-            let hsl_values = ColourNaming::to_hsl_values_from_hsl_ratios(hsl_ratios);
+fn convert_name_string(name: String) -> Option<ColourInfo> {
+    if let Ok(hex_value) = ColourNaming::to_hex_value_from_name(name.as_str()) {
+        let rgb_bytes = ColourNaming::to_rgb_bytes_from_hex_value(hex_value);
+        let hsl_ratios = ColourNaming::to_hsl_ratios_from_rgb_bytes(rgb_bytes);
+        let hsl_values = ColourNaming::to_hsl_values_from_hsl_ratios(hsl_ratios);
 
-            ColourInfo {
-                hex: ColourNaming::to_hex_string_from_rgb_bytes(rgb_bytes),
-                rgb: ColourNaming::to_rgb_string_from_rgb_bytes(rgb_bytes),
-                hsl: ColourNaming::to_hsl_string_from_hsl_values(hsl_values),
-                name: ColourNaming::to_name_from_rgb_bytes(rgb_bytes),
-            }
-        },
-        Err(err) => panic!("{:?}", err)
+        return Some(ColourInfo {
+            hex: ColourNaming::to_hex_string_from_rgb_bytes(rgb_bytes),
+            rgb: ColourNaming::to_rgb_string_from_rgb_bytes(rgb_bytes),
+            hsl: ColourNaming::to_hsl_string_from_hsl_values(hsl_values),
+            name: ColourNaming::to_name_from_rgb_bytes(rgb_bytes),
+        });
     }
+
+    None
 }
 
 #[tauri::command]
-fn convert_rgb_string(r: u32, g: u32, b: u32) -> ColourInfo {
+fn convert_rgb_string(r: u32, g: u32, b: u32) -> Option<ColourInfo> {
     let rgb_bytes = [r as u8, g as u8, b as u8];
     let hsl_ratios = ColourNaming::to_hsl_ratios_from_rgb_bytes(rgb_bytes);
     let hsl_values = ColourNaming::to_hsl_values_from_hsl_ratios(hsl_ratios);
 
-    ColourInfo {
+    Some(ColourInfo {
         hex: ColourNaming::to_hex_string_from_rgb_bytes(rgb_bytes),
         rgb: ColourNaming::to_rgb_string_from_rgb_bytes(rgb_bytes),
         hsl: ColourNaming::to_hsl_string_from_hsl_values(hsl_values),
         name: ColourNaming::to_name_from_rgb_bytes(rgb_bytes),
-    }
+    })
 }
 
 fn main() {
